5b0dace4cda6a67522734c164ff3d0e3da9bc51f
Avirag <aviragpv26@gmail.com>
2025-10-30 17:27:50 +0530

    few changed

--- server/server.js
+++ server/server.js
@@ -58,9 +58,9 @@ app.use('/api/messages', messageRouter)
 await connectDB();
 
 const PORT = process.env.PORT || 5000
-if(process.env.NODE_ENV !== "production"){
+
 server.listen(PORT, () => {
     console.log("Server is running on port " + PORT)
 })
-}
+
 export default server;
